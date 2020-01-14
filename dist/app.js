07cd50c13e55cb6c9980a0b6f533623ed311d80f
Adam Carter <adamcarter@jakatasalon.co.uk>
2020-01-14 13:52:59 +0000

    removed logo

--- dist/app.js
+++ dist/app.js
@@ -28447,8 +28447,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/adam-home/GoSites/quick-elec20/src/js/app.js */"./src/js/app.js");
-module.exports = __webpack_require__(/*! /Users/adam-home/GoSites/quick-elec20/src/app.scss */"./src/app.scss");
+__webpack_require__(/*! /Users/adam-macbook/GoSites/quick-elec20/src/js/app.js */"./src/js/app.js");
+module.exports = __webpack_require__(/*! /Users/adam-macbook/GoSites/quick-elec20/src/app.scss */"./src/app.scss");
 
 
 /***/ })
