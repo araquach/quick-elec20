36322feef9c5d9fd14b6fd7d73c1b5f0ea7b7984
Adam Carter <adamcarter@jakatasalon.co.uk>
2020-01-14 21:00:34 +0000

    Added the form

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,16 @@
 import Vue from 'vue'
 import Vuelidate from 'vuelidate'
 import Slider from "./components/Slider"
-import ContactForm from "./components/ContactForm";
-import Buefy from "buefy";
+import ContactForm from "./components/ContactForm"
+import Buefy from "buefy"
 
 Vue.use(Vuelidate)
 Vue.use(Buefy)
 
 Vue.component('slider-component', Slider)
 Vue.component('contact-form', ContactForm)
+
+window.axios = require('axios')
 
 new Vue({
     el: '#app',
