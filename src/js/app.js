ba979194a8c9d06f39c44070e8621927f56ac055
Adam Carter <adamcarter@jakatasalon.co.uk>
2020-01-14 20:03:45 +0000

    Added contact form vue component

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import Vuelidate from 'vuelidate'
 import Slider from "./components/Slider"
+import ContactForm from "./components/ContactForm";
 import Buefy from "buefy";
 
 Vue.use(Vuelidate)
 Vue.use(Buefy)
 
 Vue.component('slider-component', Slider)
+Vue.component('contact-form', ContactForm)
 
 new Vue({
     el: '#app',
---
+++ src/js/components/ContactForm.vue
@@ -0,0 +1,93 @@
+<template>
+    <form @submit.prevent="submit">
+        <div class="field">
+            <label class="label has-text-white">Full Name</label>
+            <div class="control">
+                <input class="input" v-model.trim="$v.name.$model" :class="{ 'is-danger': $v.name.$error }" placeholder="Your Full Name">
+            </div>
+            <div class="help is-danger" v-if="submitStatus === 'ERROR' && !$v.name.required">
+                Name is required
+            </div>
+        </div>
+        <div class="field">
+            <label class="label has-text-white">Email Address</label>
+            <div class="control">
+                <input class="input" :class="{ 'is-danger': $v.email.$error }" v-model.trim="$v.email.$model" placeholder="Your Email Address">
+                <div class="help is-danger" v-if="submitStatus === 'ERROR' && !$v.email.required">
+                    Email Address is required
+                </div>
+                <div class="help is-danger" v-if="submitStatus === 'ERROR' && !$v.email.email">
+                    Valid Email Address is required
+                </div>
+            </div>
+        </div>
+        <div class="field">
+            <label class="label has-text-white">Message</label>
+            <div class="control">
+                <input class="textarea" :class="{ 'is-danger': $v.message.$error }" v-model.trim="$v.message.$model" placeholder="Your Message">
+                <div class="help is-danger" v-if="submitStatus === 'ERROR' && !$v.message.required">
+                    Message is required
+                </div>
+            </div>
+        </div>
+        <br>
+        <div class="field">
+            <div class="control">
+                <button class="button is-primary" type="submit" :disabled="submitStatus === 'PENDING'">Send Message</button>
+            </div>
+            <br><br>
+            <div v-if="submitStatus === 'OK'">
+                <p class="is-size-4 has-text-primary">Thanks for messaging us! One of our team will get back to you soon.</p>
+            </div>
+        </div>
+    </form>
+</template>
+
+<script>
+    import {required, email} from 'vuelidate/lib/validators'
+    export default {
+        data() {
+            return {
+                name: '',
+                email: '',
+                message: '',
+                submitStatus: null
+            }
+        },
+
+        validations: {
+            name: { required },
+            email: { required, email },
+            message: { required }
+        },
+
+        methods:{
+            fullMessage() {
+                return `From: ${this.name}
+                Email Address: ${this.email}
+                Message: ${this.message}
+                `
+            },
+
+            submit() {
+                console.log('submit!')
+                this.$v.$touch()
+                if (this.$v.$invalid) {
+                    this.submitStatus = 'ERROR'
+                } else {
+                    axios.post('/api/sendMessage', {
+                        name: this.name,
+                        email: this.email,
+                        message: this.fullMessage()
+                    })
+                        .then(response => {
+                            this.submitStatus = 'OK'
+                        })
+                        .catch((e) => {
+                            console.error(e)
+                        })
+                }
+            }
+        }
+    }
+</script>
