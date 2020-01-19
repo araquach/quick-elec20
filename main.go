c70a0045bfdf02654cda03d5657e445c6fc4b032
Adam Carter <adam@contra-design.com>
2020-01-19 15:05:22 +0000

    Update main.go

--- main.go
+++ main.go
@@ -101,8 +101,9 @@ func apiSendMessage(w http.ResponseWriter, r *http.Request) {
 	sender := "contact@quick-elec.co.uk"
 	subject := "New Quick-Elec Enquiry"
 	body := data.Message
-	recipient := "george@quick-elec.co.uk"
-	// recipient := "adam@jakatasalon.co.uk"
+	// recipient := "george@quick-elec.co.uk"
+	recipient := "george.jones91@hotmail.co.uk"
+
 
 	// The message object allows you to add attachments and Bcc recipients
 	message := mg.NewMessage(sender, subject, body, recipient)
